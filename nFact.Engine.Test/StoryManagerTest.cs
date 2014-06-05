b54a567c59ac9e6dc5e29ec7390167a511c88624
mike_smo <mike_smo@yahoo.com>
2014-06-06 08:49:04 +1000

    StoryManagerTests

--- nFact.Engine.Test/StoryManagerTest.cs
+++ nFact.Engine.Test/StoryManagerTest.cs
@@ -12,15 +12,40 @@ namespace nFact.Engine.Test
         {
             var project = new Project("SpecTests");
             var a1 = project.CreateArtifacts("local", "1.0.0");
+            var a2 = project.CreateArtifacts("UAT", "1.0.1");
+            var a3 = project.CreateArtifacts("local", "1.0.2");
             a1.RecordTestComplete();
-
-            //project.CreateArtifacts("UAT", "1.0.1");
-            //project.CreateArtifacts("local", "1.0.2");
-            //project.CreateArtifacts("UAT", "1.0.3");
+            a2.RecordTestComplete();
+            a3.RecordTestComplete();
 
             var manager = new StoryManager();
             var results = manager.GetProjectStoryResults(project);
-            Assert.AreEqual("local", results.Environments[0].Name);
+            
+            var local = results.Environments[0];
+            var uat = results.Environments[1];
+            var localStory1 = local.Stories[0];
+            var localStory2 = local.Stories[1];
+            var uatStory1 = uat.Stories[0];
+            var uatStory2 = uat.Stories[1];
+            var testResult1a = localStory1.Results[0];
+            var testResult1b = localStory2.Results[0];
+            var testResult2a = uatStory1.Results[0];
+            var testResult2b = uatStory2.Results[0];
+            var testResult3a = localStory1.Results[1];
+            var testResult3b = localStory2.Results[1];
+
+            Assert.AreEqual("local", local.Name);
+            Assert.AreEqual("UAT", uat.Name);
+            Assert.AreEqual("12345", localStory1.Id);
+            Assert.AreEqual("1111", localStory2.Id);
+            Assert.AreEqual("12345", uatStory1.Id);
+            Assert.AreEqual("1111", uatStory2.Id);
+            Assert.AreEqual(1, testResult1a.TestRun);
+            Assert.AreEqual(1, testResult1b.TestRun);
+            Assert.AreEqual(2, testResult2a.TestRun);
+            Assert.AreEqual(2, testResult2b.TestRun);
+            Assert.AreEqual(3, testResult3a.TestRun);
+            Assert.AreEqual(3, testResult3b.TestRun);
         }
     }
 }
