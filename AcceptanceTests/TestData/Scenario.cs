834509d6e7194d257ba46c868e508fcbdb5717a5
mike_smo <mike_smo@yahoo.com>
2014-06-07 22:00:42 +1000

    Scenario Teardown with tag

---
+++ AcceptanceTests/TestData/Scenario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AcceptanceTests.Configuration;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.TestData
+{
+    public class Scenario
+    {
+        public static void SetupData(string script)
+        {
+            try
+            {
+                TestConfigurationManager.Load();
+
+                var dbFit = new DbFitHandler();
+                dbFit.Setup(script);
+            }
+            catch
+            {
+            }
+        }
+
+        public static void TeardownData(string script)
+        {
+            TeardownData(script, null);
+        }
+
+        public static void TeardownData(string script, string tag)
+        {
+            try
+            {
+                TestConfigurationManager.Load();
+
+                var scenarioInfo = ScenarioContext.Current.ScenarioInfo;
+                if (tag == null || scenarioInfo.Tags.Any(t => t == tag))
+                {
+                    var dbFit = new DbFitHandler();
+                    dbFit.Teardown(script);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
